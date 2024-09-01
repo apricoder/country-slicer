c6ca7bf38c7e41576f349629ea9585cdcee27d34
alexander <alexander.apricot@gmail.com>
2024-09-01 02:43:38 +0200

    feat: add side panel placeholder

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 import { MapContainer, TileLayer, Polygon } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import * as L from 'leaflet'
 
 import polandBorderPolygons from './polygons/pol';
 
@@ -14,15 +10,27 @@ const App = () => {
 
   return (
     <>
-      <div style={{ height: '100%', width: '100%' }}>
-        <MapContainer center={[52.237049, 19.017532]} zoom={6} style={{ height: '100%', width: '100%' }}>
-          <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          />
-          <Polygon positions={polandBorders} color="blue"/>
-        </MapContainer>
+      <div className="fx-row h-100vh">
+        <div className="side-panel fx-1 fx-col h-100">
+          <div className="title-wrapper"></div>
+        </div>
+
+        <div className="fx-1 h-100">
+          <MapContainer center={[52.237049, 19.017532]} zoom={6} style={{ height: '100%', width: '100%' }}>
+            <TileLayer
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            />
+            <>
+              {polandBorderPolygons.map((p, i) => (
+                <Polygon key={i} positions={p} color="blue"/>
+              ))}
+            </>
+          </MapContainer>
+        </div>
+
       </div>
+
     </>
   )
 };
